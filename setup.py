12ee9b6d7860bd123bfb23830b21b5c15e400533
binamra-linux <binamrapandey180@gmail.com>
2026-02-15 13:27:21 +0545

    updating version

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ long_description = readme_file.read_text(encoding="utf-8") if readme_file.exists
 
 setup(
     name="secure-crypto",
-    version="1.0.0",
+    version="1.0.1",
     author="Binamra Bikram Pandey",
     author_email="binamrapandey6@gmail.com",
     description="A cryptography library with strong security defaults for non-expert developers",
